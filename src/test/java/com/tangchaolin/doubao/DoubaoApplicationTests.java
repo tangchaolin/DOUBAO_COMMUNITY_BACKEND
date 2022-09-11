8ced2964b1cb72375fbfc7038abf473229d619c7
tcl10 <tcl1043@163.com>
2022-09-11 13:11:33 +0800

    init

---
+++ src/test/java/com/tangchaolin/doubao/DoubaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tangchaolin.doubao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoubaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
