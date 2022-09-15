7f5f8ac768772f68cbef9819e5292ad4b2b11b46
tcl10 <tcl1043@163.com>
2022-09-15 18:33:43 +0800

    帖子列表分页后端实现

--- src/main/java/com/tangchaolin/doubao/DoubaoApplication.java
+++ src/main/java/com/tangchaolin/doubao/DoubaoApplication.java
@@ -1,5 +1,6 @@
 package com.tangchaolin.doubao;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.tangchaolin.doubao.jwt.JwtAuthenticationFilter;
@@ -9,6 +10,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 @SpringBootApplication
+@MapperScan("com.tangchaolin.doubao.mapper")
 public class DoubaoApplication extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
--- src/main/java/com/tangchaolin/doubao/service/Impl/BmsBillboardImpl.java
+++ src/main/java/com/tangchaolin/doubao/service/Impl/BmsBillboardImpl.java
@@ -4,9 +4,14 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.tangchaolin.doubao.mapper.BmsBillboardMapper;
 import com.tangchaolin.doubao.model.entity.BmsBillboard;
 import com.tangchaolin.doubao.service.IBmsBillboardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BmsBillboardImpl extends ServiceImpl<BmsBillboardMapper, BmsBillboard>
 implements IBmsBillboardService
-{ }
+{
+
+
+
+}
